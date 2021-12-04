29e68d18c1c691604b313424a3f2aa00ee027413
JakubKrol <contact@itdotfocus.com>
2021-12-05 00:16:41 +0100

    Update texts

--- src/text.js
+++ src/text.js
@@ -12,13 +12,13 @@ const prepareNick = (nick) => pipe(
   textEllipsis(15),
 );
 
-const getDescriptionPrefix = ({ authorDisplayName, authorChannelUrl, likeCount }) => `Take a look at channel of ${latinize(authorDisplayName.trim())}: ${authorChannelUrl}. He has best comment under this video right now with ${likeCount} likes!`;
+const getDescriptionPrefix = ({ authorDisplayName, authorChannelUrl, likeCount }) => `Take a look at channel of ${latinize(authorDisplayName.trim())}: ${authorChannelUrl}. (S)he has best comment under this video right now with ${likeCount} likes!`;
 
-const getTitlePostfix = ({ authorDisplayName, likeCount }) => `for ${latinize(authorDisplayName.trim())} as his comments has ${likeCount} likes!`;
+const getTitlePostfix = ({ authorDisplayName, likeCount }) => `for ${latinize(authorDisplayName.trim())} as his/her comments has ${likeCount} likes!`;
 
 const prepareTitleAndDesc = (bestComment) => ({
   title: `This video is a free promo ${getTitlePostfix(bestComment)}`,
-  description: `${getDescriptionPrefix(bestComment)}\n\nThis video updates itself every 15 minutes. The most popular comment's (which is relevance by YT algorithm and number of likes) author is then showed in the title, description and in the center of the thumbnail.\n\nCheck out thumbnail of this video here: http://i3.ytimg.com/vi/FS5nNIFuH_4/hqdefault.jpg\n\nIn the background of the thumbnail you can see 84 of newest comments authors' avatars!\n\nThe video itself is explanation of how it works.`,
+  description: `${getDescriptionPrefix(bestComment)}\n\nThis video updates itself every 15 minutes. The most popular comment's (which is relevance by YT algorithm and number of likes) author is then showed in the title, description and in the center of the thumbnail.\n\nCheck out thumbnail of this video here: http://i3.ytimg.com/vi/FS5nNIFuH_4/hqdefault.jpg\n\nIn the background of the thumbnail you can see 84 of newest comments authors' avatars!\n\nThe video itself is explanation of how it works.\n\nIF YOU ARE THE CODER DON'T DIRECTLY USE CODE IN THE VIDEO! You may find much better version here: https://github.com/Ami777/ThisVideoInFreePromo`,
 });
 
 module.exports = {
