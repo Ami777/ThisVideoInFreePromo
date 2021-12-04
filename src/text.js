942f4391a441ad5c980a542eb0e37f0196c70006
JakubKrol <contact@itdotfocus.com>
2021-12-05 00:18:09 +0100

    Update texts even more

--- src/text.js
+++ src/text.js
@@ -1,5 +1,6 @@
 const latinize = require('latinize');
 const pipe = require('pipe-functions');
+const { Config } = require('../data/config');
 
 const textEllipsis = (maxLen) => (s) => {
   const str = s.trim();
@@ -18,7 +19,7 @@ const getTitlePostfix = ({ authorDisplayName, likeCount }) => `for ${latinize(au
 
 const prepareTitleAndDesc = (bestComment) => ({
   title: `This video is a free promo ${getTitlePostfix(bestComment)}`,
-  description: `${getDescriptionPrefix(bestComment)}\n\nThis video updates itself every 15 minutes. The most popular comment's (which is relevance by YT algorithm and number of likes) author is then showed in the title, description and in the center of the thumbnail.\n\nCheck out thumbnail of this video here: http://i3.ytimg.com/vi/FS5nNIFuH_4/hqdefault.jpg\n\nIn the background of the thumbnail you can see 84 of newest comments authors' avatars!\n\nThe video itself is explanation of how it works.\n\nIF YOU ARE THE CODER DON'T DIRECTLY USE CODE IN THE VIDEO! You may find much better version here: https://github.com/Ami777/ThisVideoInFreePromo`,
+  description: `${getDescriptionPrefix(bestComment)}\n\nThis video updates itself every 15 minutes. The most popular comment's (which is relevance by YT algorithm and number of likes) author is then showed in the title, description and in the center of the thumbnail.\n\nCheck out thumbnail of this video here: http://i3.ytimg.com/vi/${Config.youtube.videoId}/hqdefault.jpg\n\nIn the background of the thumbnail you can see 84 of newest comments authors' avatars!\n\nThe video itself is explanation of how it works.\n\nIF YOU ARE THE CODER DON'T DIRECTLY USE CODE IN THE VIDEO! You may find much better version here: https://github.com/Ami777/ThisVideoInFreePromo`,
 });
 
 module.exports = {
